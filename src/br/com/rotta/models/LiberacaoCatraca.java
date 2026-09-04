2e149c85771aa62dc6aa839c2b33367733e30771
Guilherme <guimmalmeidaads@gmail.com>
2026-09-04 17:23:03 -0300

    feat: adiciona classes LiberacaoCatraca e RottaCard, incrementa construtores, métodos e getters

--- src/br/com/rotta/models/LiberacaoCatraca.java
+++ src/br/com/rotta/models/LiberacaoCatraca.java
@@ -1,4 +1,51 @@
 package br.com.rotta.models;
 
+import br.com.rotta.enums.MetodoLiberacao;
+import br.com.rotta.enums.StatusLiberacao;
+
+import java.time.LocalDateTime;
+
 public class LiberacaoCatraca {
+
+    //ATRIBUTOS
+    private int id;
+    private MetodoLiberacao tipoMetodo;
+    private LocalDateTime dataLiberacao;
+    private StatusLiberacao status;
+
+    //CONSTRUTOR
+    public LiberacaoCatraca(int id) {
+        this.id = id;
+        this.dataLiberacao = LocalDateTime.now();
+    }
+
+    //MÉTODOS
+    public boolean liberarViaNFC(RottaCard cartao) {
+        this.tipoMetodo = MetodoLiberacao.NFC;
+        System.out.println("Aproximando o Rotta Card no validador...");
+        if (cartao.isAtivo() && cartao.getCarteira().consultarSaldo() > 0) {
+            this.status = StatusLiberacao.VALIDADA;
+            System.out.println("Catraca libereada via NFC! Boa viagem!");
+            return true;
+        }
+        this.status = StatusLiberacao.NEGADA;
+        System.out.println("Liberação negada. Verifique seu saldo ou o estado do cartão.");
+        return false;
+    }
+
+    public boolean liberarViaQRCode(Resgate resgate) {
+        this.tipoMetodo = MetodoLiberacao.QRCODE;
+        System.out.println("Lendo o QR Code no validador...");
+        if (resgate.validarQRCode()) {
+            this.status = StatusLiberacao.VALIDADA;
+            System.out.println("Catraca liberada via QR Code! Boa viagem!");
+            return true;
+        }
+        this.status = StatusLiberacao.NEGADA;
+        return false;
+    }
+    //GETTER
+    public StatusLiberacao getStatus() {
+        return status;
+    }
 }
--- src/br/com/rotta/models/RottaCard.java
+++ src/br/com/rotta/models/RottaCard.java
@@ -1,4 +1,32 @@
 package br.com.rotta.models;
 
 public class RottaCard {
+    //ATRIBUTOS
+    private int id;
+    private String codigoNfc;
+    private boolean ativo;
+    private Carteira carteira;
+
+    //CONSTRUTOR
+    public RottaCard(int id, String codigoNfc, Carteira carteira) {
+        this.id = id;
+        this.codigoNfc = codigoNfc;
+        this.carteira = carteira;
+        this.ativo = true;
+    }
+
+    //MÉTODOS
+    public String identificar() {
+        System.out.println("Lendo cartão NFC...");
+        return this.codigoNfc;
+    }
+
+    //GETTERS
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public Carteira getCarteira() {
+        return carteira;
+    }
 }
