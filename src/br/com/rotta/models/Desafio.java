c61df6df6467304659ffde6cc4af280f0c2a7f2b
Guilherme <guimmalmeidaads@gmail.com>
2026-09-04 18:07:14 -0300

    feat: adiciona classes Parceiro e Desafio, incrementa construtores, métodos e getters

--- src/br/com/rotta/models/Desafio.java
+++ src/br/com/rotta/models/Desafio.java
@@ -1,4 +1,51 @@
 package br.com.rotta.models;
 
+import br.com.rotta.enums.FormatoMidia;
+
 public class Desafio {
+    private int id;
+    private String nome;
+    private FormatoMidia tipoFormato;
+    private int tempoMaxVideo;
+    private int pontosDesafio;
+    private boolean ativo;
+    private Parceiro parceiro;
+
+    //CONSTRUTOR
+
+    public Desafio(int id, String nome, FormatoMidia tipoFormato, int pontosDesafio, Parceiro parceiro, boolean ativo) {
+        this.id = id;
+        this.nome = nome;
+        this.tipoFormato = tipoFormato;
+        this.pontosDesafio = pontosDesafio;
+        this.parceiro = parceiro;
+        this.ativo = true;
+    }
+
+    //MÉTODOS
+    public void exibirDesafio() {
+        System.out.println("-----------------------------------------" +
+                            "Desafio: " + nome +
+                            "Formato exigido: " + tipoFormato +
+                            "Recompensa: " + pontosDesafio + "pontos" +
+                            "Patrocinado por: " + parceiro.getNome() +
+                            "-----------------------------------------");
+    }
+
+    //GETTERS
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getPontosDesafio() {
+        return pontosDesafio;
+    }
+
+    public FormatoMidia getTipoFormato() {
+        return tipoFormato;
+    }
 }
--- src/br/com/rotta/models/Parceiro.java
+++ src/br/com/rotta/models/Parceiro.java
@@ -1,4 +1,46 @@
 package br.com.rotta.models;
 
+import br.com.rotta.enums.TipoParceiro;
+
 public class Parceiro {
+
+    //ATRIBUTOS
+    private int id;
+    private String nome;
+    private TipoParceiro tipo;
+    private String documento;
+    private boolean ativo;
+
+    //CONSTRUTOR
+    public Parceiro(int id, String nome, TipoParceiro tipo, String documento, boolean ativo) {
+        this.id = id;
+        this.nome = nome;
+        this.tipo = tipo;
+        this.documento = documento;
+        this.ativo = ativo;
+    }
+
+    //MÉTODOS
+    public void cadastrar() {
+        System.out.println("Parceiro " + nome + "(" + tipo + ") cadastrado com sucesso na Rotta!");
+    }
+
+    public void ativar() {
+        this.ativo = true;
+        System.out.println("Parceiro " + nome + " está ativo e patrocinando desafios.");
+    }
+
+    public void desativar() {
+        this.ativo = false;
+        System.out.println("Parceiro " + nome + " foi desativado.");
+    }
+
+    //GETTERS
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
