91553171e93cb4b74f1b3ccfb4459e747ced4fe7
Guilherme <guimmalmeidaads@gmail.com>
2026-09-04 18:51:45 -0300

    feat: adiciona classe CreditoPontos com inserção de Atributos, Construtor e Método

--- src/br/com/rotta/models/CreditoPontos.java
+++ src/br/com/rotta/models/CreditoPontos.java
@@ -1,4 +1,25 @@
 package br.com.rotta.models;
 
 public class CreditoPontos {
+    //ATRIBUTOS
+    private Pontuacao pontuacao;
+    private Carteira carteira;
+
+    //CONSTRUTOR
+    public CreditoPontos(Pontuacao pontuacao, Carteira carteira) {
+        this.pontuacao = pontuacao;
+        this.carteira = carteira;
+    }
+
+    //METODO
+    public void executar() {
+        double pontos = pontuacao.getPontosAtribuidos();
+        if (pontos > 0) {
+            System.out.println("Creditando pontos na carteira digital...");
+            carteira.creditarPontos(pontos);
+        } else {
+            System.out.println("Nenhum ponto a creditar nesta operação.");
+        }
+    }
+
 }
